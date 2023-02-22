d480ab69ac85f15dc775a89fb9f2a2eaf574e4c8
Mara <mara@autochthe.net>
2023-02-22 01:51:41 -0800

    Begin inflate

---
+++ local/inflate.py
@@ -0,0 +1,119 @@
+from enum import Enum
+from typing import Mapping
+
+import torch
+import torch.nn as nn
+
+from . import model
+
+class InflateAction(Enum):
+    SET_ZERO = Enum.auto()
+
+
+@torch.no_grad()
+def inflate(network0: nn.Module, network1: nn.Module, mapping: Mapping[str, str]):
+    """Initialize network1 via inflating network0
+
+    Args:
+        network0: Network to inflate.  Not mutated.
+        network1: Network to initialize.  Mutated.
+    """
+
+    def action(input: InflateAction, output: str):
+        match input:
+            case InflateAction.SET_ZERO:
+                network1.get_parameter(output).zero_()
+
+
+    # Random initialization
+    model.reset(network1)
+
+    for output, input in mapping.items():
+        if isinstance(input, str):
+            # Copy parameters
+            network1.get_parameter(output).copy_(network0.get_parameter(input))
+        elif isinstance(input, InflateAction):
+            action(input, output)
+
+
+def inflate_resnet50_resnet152(resnet50: resnet.ResNet, resnet152: resnet.ResNet):
+    inflate(resnet50, resnet152, {
+        # Input Layer
+        'conv1.weight': 'conv1.weight',
+        'bn1.weight': 'bn1.weight',
+        'bn1.bias': 'bn1.bias',
+
+        # 'layer1.0.conv1.weight',
+        # 'layer1.0.bn1.weight',
+        # 'layer1.0.bn1.bias',
+        # 'layer1.0.conv2.weight',
+        # 'layer1.0.bn2.weight',
+        # 'layer1.0.bn2.bias',
+        # 'layer1.0.conv3.weight',
+        # 'layer1.0.bn3.weight',
+        # 'layer1.0.bn3.bias',
+        # 'layer1.0.downsample.0.weight',
+        # 'layer1.0.downsample.1.weight',
+        # 'layer1.0.downsample.1.bias',
+        # 'layer1.1.conv1.weight',
+        # 'layer1.1.bn1.weight',
+        # 'layer1.1.bn1.bias',
+        # 'layer1.1.conv2.weight',
+        # 'layer1.1.bn2.weight',
+        # 'layer1.1.bn2.bias',
+        # 'layer1.1.conv3.weight',
+        # 'layer1.1.bn3.weight',
+        # 'layer1.1.bn3.bias',
+        # 'layer1.2.conv1.weight',
+        # 'layer1.2.bn1.weight',
+        # 'layer1.2.bn1.bias',
+        # 'layer1.2.conv2.weight',
+        # 'layer1.2.bn2.weight',
+        # 'layer1.2.bn2.bias',
+        # 'layer1.2.conv3.weight',
+        # 'layer1.2.bn3.weight',
+        # 'layer1.2.bn3.bias',
+        # 'layer2.0.conv1.weight',
+        # 'layer2.0.bn1.weight',
+        # 'layer2.0.bn1.bias',
+        # 'layer2.0.conv2.weight',
+        # 'layer2.0.bn2.weight',
+        # 'layer2.0.bn2.bias',
+        # 'layer2.0.conv3.weight',
+        # 'layer2.0.bn3.weight',
+        # 'layer2.0.bn3.bias',
+        # 'layer2.0.downsample.0.weight',
+        # 'layer2.0.downsample.1.weight',
+        # 'layer2.0.downsample.1.bias',
+        # 'layer2.1.conv1.weight',
+        # 'layer2.1.bn1.weight',
+        # 'layer2.1.bn1.bias',
+        # 'layer2.1.conv2.weight',
+        # 'layer2.1.bn2.weight',
+        # 'layer2.1.bn2.bias',
+        # 'layer2.1.conv3.weight',
+        # 'layer2.1.bn3.weight',
+        # 'layer2.1.bn3.bias',
+        # 'layer2.2.conv1.weight',
+        # 'layer2.2.bn1.weight',
+        # 'layer2.2.bn1.bias',
+        # 'layer2.2.conv2.weight',
+        # 'layer2.2.bn2.weight',
+        # 'layer2.2.bn2.bias',
+        # 'layer2.2.conv3.weight',
+        # 'layer2.2.bn3.weight',
+        # 'layer2.2.bn3.bias',
+        # 'layer2.3.conv1.weight',
+        # 'layer2.3.bn1.weight',
+        # 'layer2.3.bn1.bias',
+        # 'layer2.3.conv2.weight',
+        # 'layer2.3.bn2.weight',
+        # 'layer2.3.bn2.bias',
+        # 'layer2.3.conv3.weight',
+        # 'layer2.3.bn3.weight',
+        # 'layer2.3.bn3.bias',
+
+        # Output Layer
+        'fc.weight': 'fc.weight',
+        'fc.bias': 'fc.bias',
+    })
--- local/resnet.py
+++ local/resnet.py
@@ -1,5 +1,4 @@
 from torch.hub import load
-import torch.nn as nn
 from torchvision.models import resnet
 
 
@@ -8,12 +7,3 @@ resnet34 = lambda: load('pytorch/vision:v0.10.0', 'resnet34', weights=resnet.Res
 resnet50 = lambda: load('pytorch/vision:v0.10.0', 'resnet50', weights=resnet.ResNet50_Weights.IMAGENET1K_V1)
 resnet101 = lambda: load('pytorch/vision:v0.10.0', 'resnet101', weights=resnet.ResNet101_Weights.IMAGENET1K_V1)
 resnet152 = lambda: load('pytorch/vision:v0.10.0', 'resnet152', weights=resnet.ResNet152_Weights.IMAGENET1K_V1)
-
-def inflate_resnet50_resnet152(resnet50: resnet.ResNet, resnet152: resnet.ResNet):
-    """Initialize Resnet152 via inflating pretrained Resnet50
-
-    Args:
-        resnet50: Resnet to inflate.  Not mutated.
-        resnet152: Resnet to initialize.  Mutated.
-    """
-    return
