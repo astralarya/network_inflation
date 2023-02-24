83f046ed31ac2a4db3dbe78ce7ca78520b508291
Mara Kim <mara@autochthe.net>
2023-02-24 01:55:19 -0800

    setup tpu

--- scripts/setup_tpu.bash
+++ scripts/setup_tpu.bash
@@ -7,11 +7,16 @@ sudo apt-get install -y screen git
 echo 'export XRT_TPU_CONFIG="localservice;-1;localhost:51011"' >> ~/.bashrc
 source ~/.bashrc
 
-# git clone git@github.com:astralarya/network_inflation.git
-# pip install -r requirements.txt
-
 # https://cloud.google.com/compute/docs/disks/add-persistent-disk
-# sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
-# sudo mkdir /mnt/imagenet
-# sudo mount -o discard,defaults /dev/sdb /mnt/imagenet
+sudo mkdir /mnt/imagenet
+sudo mount -o discard,defaults /dev/sdb /mnt/imagenet
+
+
+ssh-keygen
+cat ~/.ssh/id_rsa.pub
+
+git clone git@github.com:astralarya/network_inflation.git
+cd network_inflation
+pip install -r requirements.txt
+
  
