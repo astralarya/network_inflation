ea24de26db781520242349c618e12e7408fd77bc
Mara Kim <mara@autochthe.net>
2023-02-24 01:47:53 -0800

    modify bashrc

--- scripts/setup_tpu.bash
+++ scripts/setup_tpu.bash
@@ -4,7 +4,8 @@
 sudo apt-get update
 sudo apt-get install -y screen git
 
-export XRT_TPU_CONFIG="localservice;0;localhost:51011"
+echo 'export XRT_TPU_CONFIG="localservice;-1;localhost:51011"' >> ~/.bashrc
+source ~/.bashrc
 
 # git clone git@github.com:astralarya/network_inflation.git
 # pip install -r requirements.txt
