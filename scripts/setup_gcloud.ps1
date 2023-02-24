e5dc2582ac13037fa268567bf27b40a0ae647848
Mara Kim <mara@autochthe.net>
2023-02-23 20:21:03 -0800

    tpu setup

--- scripts/setup_gcloud.ps1
+++ scripts/setup_gcloud.ps1
@@ -2,10 +2,26 @@
 
 $INSTANCE_NAME = "tpu-1"
 
-$GCLOUD_ZONE = "us-central1-f"
-$TPU_TYPE = "v2-8"
+$GCLOUD_ZONE = "europe-west4-a"
+$TPU_TYPE = "v3-8"
 $TPU_VERSION = "tpu-vm-pt-1.13"
-$GCLOUD_DISK = "imagenet--$GCLOUD_ZONE"
+$GCLOUD_DISK = "imagenet--$INSTANCE_NAME"
+$GCLOUD_DISK_SIZE = "150GB"
+$GCLOUD_SNAPSHOT = "imagenet-1k--eu"
 
-gcloud compute tpus tpu-vm create "$INSTANCE_NAME" "--zone=$GCLOUD_ZONE" "--accelerator-type=$TPU_TYPE" "--version=$TPU_VERSION"
-gcloud alpha compute tpus tpu-vm attach-disk "$INSTANCE_NAME" "--zone=$GCLOUD_ZONE"  "--disk=$GCLOUD_DISK" --mode=read-only
+gcloud compute disks create "$GCLOUD_DISK" `
+    "--zone=$GCLOUD_ZONE" `
+    "--size=$GCLOUD_DISK_SIZE" `
+    "--source-snapshot=$GCLOUD_SNAPSHOT"
+
+gcloud compute tpus tpu-vm create "$INSTANCE_NAME" `
+    "--zone=$GCLOUD_ZONE" `
+    "--accelerator-type=$TPU_TYPE" `
+    "--version=$TPU_VERSION"
+
+gcloud alpha compute tpus tpu-vm attach-disk "$INSTANCE_NAME" `
+    "--zone=$GCLOUD_ZONE" `
+    "--disk=$GCLOUD_DISK" `
+    --mode=read-only
+
+#  gcloud compute tpus tpu ssh "$INSTANCE_NAME" "--zone=$GCLOUD_ZONE" --ssh-flag="-4 -L 9001:localhost:9001"
--- scripts/setup_tpu.bash
+++ scripts/setup_tpu.bash
@@ -10,3 +10,8 @@ export XRT_TPU_CONFIG="localservice;0;localhost:51011"
 # pip install -r requirements.txt
 
 # https://cloud.google.com/compute/docs/disks/add-persistent-disk
+# sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
+# sudo mkdir /mnt/imagenet
+# sudo mount -o discard,defaults /dev/sdb /mnt/imagenet
+# sudo chmod a+w /mnt/disks/imagenet
+ 
