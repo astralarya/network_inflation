93b470dcb3670815e759c8b16b8ed39d21fdec6a
Mara Kim <mara@autochthe.net>
2023-02-23 19:08:06 -0800

    Fix device

--- local/device.py
+++ local/device.py
@@ -8,10 +8,8 @@ except ImportError:
 
 if torch.backends.mps.is_available():
     device = torch.device("mps")
-elif xla is not None and xla.xla_device("TPU") is not None:
-    device = xla.xla_device("TPU")
-elif xla is not None and xla.xla_device("GPU") is not None:
-    device = xla.xla_device("GPU")
+elif xla is not None and xla.xla_device() is not None:
+    device = xla.xla_device()
 elif torch.cuda.is_available():
     device = torch.device(f"cuda:{torch.cuda.current_device()}")
 else:
