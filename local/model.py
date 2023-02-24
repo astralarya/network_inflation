71bec0bde046fa87deb7745c1ecfbf65128698b6
Mara Kim <mara@autochthe.net>
2023-02-23 22:10:03 -0800

    Update model code

--- local/model.py
+++ local/model.py
@@ -10,73 +10,73 @@ import torch.nn as nn
 from .device import device
 
 
-out_dir = Path("models")
-out_dir.mkdir(exist_ok=True)
+default_dir = "models"
+Path(default_dir).mkdir(exist_ok=True)
 
 
-def get_epoch(name: str, epoch: int = None):
+def get_epoch(name: str, epoch: int = None, model_dir: str = default_dir):
     if epoch is not None:
-        save_paths = glob.glob(f"{out_dir}/{name}.{epoch:08}.pkl")
+        save_paths = glob.glob(f"{model_dir}/{name}.{epoch:08}.pkl")
     else:
-        save_paths = glob.glob(f"{out_dir}/{name}.{'[0-9]'*8}.pkl")
+        save_paths = glob.glob(f"{model_dir}/{name}.{'[0-9]'*8}.pkl")
         save_paths.sort(reverse=True)
     save_path = next(iter(save_paths), None)
     return (
-        int(save_path[len(f"{out_dir}/{name}.") :].split(".")[0])
+        int(save_path[len(f"{model_dir}/{name}.") :].split(".")[0])
         if save_path is not None
         else None
     )
 
 
-def write_log(name: str, data: Optional[str] = None):
+def write_log(name: str, data: Optional[str] = None, model_dir: str = default_dir):
     if data is not None:
-        with out_dir.joinpath(f"{name}.log").open("a") as logfile:
+        with (Path(model_dir) / f"{name}.log").open("a") as logfile:
             logfile.write(data)
 
 
-def write_record(name: str, part: str, data: Optional[str] = None):
-    write_log(f"{name}.__{part}__", data)
+def write_record(name: str, part: str, data: Optional[str] = None, model_dir: str = default_dir):
+    write_log(f"{name}.__{part}__", data, model_dir)
 
 
-def save(module: nn.Module, name: str, epoch: int):
-    save_path = out_dir.joinpath(f"{name}.{epoch:08}.pkl")
+def save(module: nn.Module, name: str, epoch: int, model_dir: str = default_dir):
+    save_path = Path(model_dir) / f"{name}.{epoch:08}.pkl"
     with save_path.open("wb") as save_file:
         print(f"Saving `{save_path}`")
         torch.save(module.state_dict(), save_file)
 
 
 def save_state(
-    modules: Mapping[Optional[str], nn.Module], name: str, epoch: int, log: str = None
+    modules: Mapping[Optional[str], nn.Module], name: str, epoch: int, log: str = None, model_dir: str = default_dir
 ):
-    write_log(log)
+    write_log(log, model_dir)
     for key, value in modules.items():
-        save(value, name if key is None else f"{name}.__{key}__", epoch)
+        save(value, name if key is None else f"{name}.__{key}__", epoch, model_dir)
 
 
-def load(module: nn.Module, name: str, epoch: int = None):
-    epoch = get_epoch(name, epoch)
+def load(module: nn.Module, name: str, epoch: int = None, model_dir: str = default_dir):
+    epoch = get_epoch(name, epoch, model_dir)
     if epoch is None:
         return None
-    save_path = f"{out_dir}/{name}.{epoch:08}.pkl"
+    save_path = Path(model_dir) / f"{name}.{epoch:08}.pkl"
     print(f"Loading `{save_path}`")
     module.load_state_dict(torch.load(save_path, map_location=device))
     return epoch
 
 
 def load_state(
-    modules: Mapping[Optional[str], nn.Module], name: str, epoch: int = None
+    modules: Mapping[Optional[str], nn.Module], name: str, epoch: int = None, model_dir: str = default_dir
 ):
     if None in modules:
-        epoch = get_epoch(name, epoch)
+        epoch = get_epoch(name, epoch, model_dir)
     for key, value in modules.items():
-        load_epoch = load(value, name if key is None else f"{name}.__{key}__", epoch)
+        load_epoch = load(value, name if key is None else f"{name}.__{key}__", epoch, model_dir)
         if epoch is not None and load_epoch != epoch:
             raise Exception(f"Missing state `{name}.__{key}__.{epoch}.pkl`")
     if epoch is not None:
         print(f"Resuming from epoch {epoch}")
     else:
         print("Saving initial state as epoch 0")
-        save_state(modules, name, 0)
+        save_state(modules, name, 0, model_dir)
     return epoch
 
 
@@ -94,12 +94,13 @@ def clone(module: nn.Module):
     return copy.deepcopy(module)
 
 
-def clean(name: str):
+def clean(name: str, model_dir: str = default_dir):
+    model_dir = Path(model_dir)
     save_paths = [
-        *glob.glob(f"{out_dir}/{name}.{'[0-9]'*8}.pkl"),
-        *glob.glob(f"{out_dir}/{name}.__*__.{'[0-9]'*8}.pkl"),
-        *glob.glob(f"{out_dir}/{name}.log"),
-        *glob.glob(f"{out_dir}/{name}.__*__.log"),
+        *glob.glob(f"{model_dir}/{name}.{'[0-9]'*8}.pkl"),
+        *glob.glob(f"{model_dir}/{name}.__*__.{'[0-9]'*8}.pkl"),
+        *glob.glob(f"{model_dir}/{name}.log"),
+        *glob.glob(f"{model_dir}/{name}.__*__.log"),
     ]
     for path in save_paths:
         print(f"Deleting `{path}`")
