b5d328ee3b5c73dfe79ac6459153018bc5b30163
Mara Kim <mara@autochthe.net>
2023-02-26 05:43:31 -0800

    typing

--- local/model.py
+++ local/model.py
@@ -2,7 +2,7 @@ import copy
 import glob
 import os
 from pathlib import Path
-from typing import Callable, Mapping, Optional
+from typing import Any, Callable, Mapping, Optional
 
 import torch
 import torch.nn as nn
