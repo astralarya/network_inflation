702e0c05f9b06ea7ae3e609e1b0e9b5c22e36820
Mara Kim <mara@autochthe.net>
2023-02-26 01:58:55 -0800

    Change to bash

---
+++ scripts/setup_gcloud.bash
@@ -0,0 +1,32 @@
+# Run on local dev
+
+INSTANCE_NAME="tpu-1"
+
+GCLOUD_ZONE="us-central1-f"
+TPU_TYPE="v2-8"
+TPU_VERSION="tpu-vm-pt-1.13"
+GCLOUD_DISK="imagenet--$INSTANCE_NAME"
+GCLOUD_DISK_SIZE="150GB"
+GCLOUD_SNAPSHOT="imagenet-1k"
+
+gcloud compute tpus tpu-vm create "$INSTANCE_NAME" \
+    "--zone=$GCLOUD_ZONE" \
+    "--accelerator-type=$TPU_TYPE" \
+    "--version=$TPU_VERSION"
+
+gcloud compute disks create "$GCLOUD_DISK" \
+    "--zone=$GCLOUD_ZONE" \
+    "--size=$GCLOUD_DISK_SIZE" \
+    "--source-snapshot=$GCLOUD_SNAPSHOT"
+
+gcloud alpha compute tpus tpu-vm attach-disk "$INSTANCE_NAME" \
+    "--zone=$GCLOUD_ZONE" \
+    "--disk=$GCLOUD_DISK" \
+    --mode=read-only
+
+gcloud compute tpus tpu-vm ssh "$INSTANCE_NAME" \
+    "--zone=$GCLOUD_ZONE"
+
+# gcloud compute tpus tpu-vm ssh "$INSTANCE_NAME" \
+#     "--zone=$GCLOUD_ZONE" \
+#     --ssh-flag="-4 -L 9001:localhost:9001"
