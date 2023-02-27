0b7df0016cd2fe7c0b15223f68e84cc25377e59f
Mara Kim <mara@autochthe.net>
2023-02-26 16:35:37 -0800

    Nicer interface

--- val_network.py
+++ val_network.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument('name')
 parser.add_argument('--network', choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'])
 parser.add_argument('--batch_size', default=64, type=int)
-parser.add_argument('--epoch', default="all", type=lambda x: x if x in ["none", "all"] else int(x))
+parser.add_argument('--epoch', default="all", type=lambda x: x if x in ["pre", "init", "all"] else int(x))
 args = parser.parse_args()
 
 
@@ -25,7 +25,12 @@ if network is None:
 
 val_data = imagenet.val_data("/mnt/imagenet/imagenet-1k/val/")
 
-if args.epoch == "all":
+if args.epoch == "pre":
+    imagenet.run_val(network, name, val_data, batch_size=args.batch_size)
+elif args.epoch == "init":
+    model.reset(network)
+    imagenet.run_val(network, name, val_data, batch_size=args.batch_size)
+elif args.epoch == "all":
     imagenet.run_val(network, name, val_data, batch_size=args.batch_size)
 elif type(args.epoch) == "int":
     imagenet.val_epoch(network, name, val_data, args.epoch, batch_size=args.batch_size)
