fa2441ba12fdd6740b6ae1227abbfc6ce42ce784
Mara Kim <mara@autochthe.net>
2023-02-26 16:49:37 -0800

    make val output nicer

--- val_network.py
+++ val_network.py
@@ -7,7 +7,7 @@ parser.add_argument('name')
 parser.add_argument('--network', choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'])
 parser.add_argument('--inflate', choices=['resnet50', 'resnet101'])
 parser.add_argument('--batch_size', default=64, type=int)
-parser.add_argument('--epoch', default="all", type=lambda x: x if x in ["pre", "init", "all"] else int(x))
+parser.add_argument('--epoch', default="pre", type=lambda x: x if x in ["pre", "init", "all"] else int(x))
 args = parser.parse_args()
 
 
@@ -32,15 +32,21 @@ if args.inflate is not None and inflate_source is None:
 
 val_data = imagenet.val_data("/mnt/imagenet/imagenet-1k/val/")
 
+print(f"Validating network {args.network}")
 if args.epoch == "pre":
     if args.inflate is not None:
         inflate.resnet(inflate_source, network)
+    else:
+        print("Using pretrained")
     imagenet.run_val(network, name, val_data, batch_size=args.batch_size)
 elif args.epoch == "init":
+    print("Resetting model")
     model.reset(network)
     imagenet.run_val(network, name, val_data, batch_size=args.batch_size)
 elif args.epoch == "all":
+    print("Validating all epochs")
     imagenet.run_val(network, name, val_data, batch_size=args.batch_size)
 elif type(args.epoch) == "int":
+    print(f"Validating epoch {args.epoch}")
     imagenet.val_epoch(network, name, val_data, args.epoch, batch_size=args.batch_size)
 
