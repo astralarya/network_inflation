c48991b5ebcebce07ec761a2734aa464d4f4494b
Mara Kim <mara@autochthe.net>
2023-02-27 13:15:39 -0800

    Respect model dir

--- val_network.py
+++ val_network.py
@@ -5,11 +5,11 @@ from pathlib import Path
 parser = argparse.ArgumentParser(
     prog="ResNet validation script"
 )
-parser.add_argument('name')
-parser.add_argument('--network', choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'])
+parser.add_argument('network', choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'])
 parser.add_argument('--inflate', choices=['resnet50', 'resnet101'])
 parser.add_argument('--batch_size', default=64, type=int)
 parser.add_argument('--epoch', default="pre", type=lambda x: x if x in ["pre", "init", "all"] else int(x))
+parser.add_argument('--model_dir', default="models", type=Path)
 parser.add_argument('--imagenet_path', default=environ.get("IMAGENET_PATH", "/mnt/imagenet/imagenet-1k"), type=Path)
 args = parser.parse_args()
 
@@ -20,35 +20,38 @@ from local import model
 from local import resnet
 
 
-name = args.name
-network = getattr(resnet, args.network, lambda: None)()
-inflate_source = getattr(resnet, args.network, lambda: None)()
+name = args.network
+if args.finetune is True:
+    name = f"{name}--finetune"
+if args.inflate is not None:
+    name = f"{name}--inflate-{args.inflate}"
 
+network = getattr(resnet, args.network, lambda: None)()
 if network is None:
     print(f"Unknown network: {args.network}")
     exit(1)
 
-if args.inflate is not None and inflate_source is None:
-    print(f"Unknown network: {args.inflate}")
-    exit(1)
-
-
 val_data = imagenet.val_data(args.imagenet_path / "val")
 
-print(f"Validating network {args.network}")
+print(f"Validating model {name}")
 if args.epoch == "pre":
     if args.inflate is not None:
+        inflate_source = getattr(resnet, args.inflate, lambda: None)()
+        if inflate_source is None:
+            print(f"Unknown network: {args.inflate}")
+            exit(1)
+        print(f"Inflating network ({args.network}) from {args.inflate}")
         inflate.resnet(inflate_source, network)
     else:
-        print("Using pretrained")
+        print("Using pretrained weights")
 elif args.epoch == "init":
-    print("Resetting model")
+    print("Resetting network")
     model.reset(network)
 
 if args.epoch == "all":
     print("Validating all epochs")
-    imagenet.run_val(network, name, val_data, batch_size=args.batch_size)
+    imagenet.run_val(network, args.model_dir / name, val_data, batch_size=args.batch_size)
 else:
     print(f"Validating epoch {args.epoch}")
-    imagenet.val_epoch(network, name, val_data, args.epoch, batch_size=args.batch_size)
+    imagenet.val_epoch(network, args.model_dir / name, val_data, args.epoch, batch_size=args.batch_size)
 
