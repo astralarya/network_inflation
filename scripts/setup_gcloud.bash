93621f1e5f61763c5112adb34f17a08a21246486
Mara Kim <mara@autochthe.net>
2023-02-27 08:53:21 -0800

    europe

--- scripts/setup_gcloud.bash
+++ scripts/setup_gcloud.bash
@@ -1,13 +1,13 @@
 # Run on local dev
 
-INSTANCE_NAME="tpu-1"
+INSTANCE_NAME="tpu-5-eu"
 
-GCLOUD_ZONE="us-central1-f"
-TPU_TYPE="v2-8"
+GCLOUD_ZONE="europe-west4-a"
+TPU_TYPE="v3-8"
 TPU_VERSION="tpu-vm-pt-1.13"
 GCLOUD_DISK="imagenet--$INSTANCE_NAME"
 GCLOUD_DISK_SIZE="150GB"
-GCLOUD_SNAPSHOT="imagenet-1k"
+GCLOUD_SNAPSHOT="imagenet-1k--eu"
 
 gcloud compute tpus tpu-vm create "$INSTANCE_NAME" \
     "--zone=$GCLOUD_ZONE" \
