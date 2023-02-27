837b44858cfaf6bcf7c00897c6024825616c831e
Mara Kim <mara@autochthe.net>
2023-02-27 08:29:13 -0800

    Divergence

--- run_divergence.py
+++ run_divergence.py
@@ -5,6 +5,8 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('network0', choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'])
 parser.add_argument('network1', choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'])
+parser.add_argument('--reset0', action='store_true')
+parser.add_argument('--reset1', action='store_true')
 parser.add_argument('--batch_size', default=64, type=int)
 parser.add_argument('--num_workers', default=8, type=int)
 args = parser.parse_args()
@@ -12,6 +14,7 @@ args = parser.parse_args()
 
 from local import inflate
 from local import imagenet
+from local import model
 from local import resnet
 
 
@@ -25,18 +28,28 @@ if network1 is None:
     print(f"Unknown network: {args.network1}")
     exit(1)
 
+if args.reset0 is None:
+    print(f"Reset network: {args.network0}")
+    model.reset(network0)
+if args.reset1 is None:
+    print(f"Reset network: {args.network1}")
+    model.reset(network1)
+
+name0 = "init-" if args.reset0 else "" + args.network0
+name1 = "init-" if args.reset1 else "" + args.network1
+
 
 train_data = imagenet.train_data()
 
-print(f"Self-divergence: {args.network1}")
+print(f"Self-divergence: {name1}")
 imagenet.divergence(network1, network1, train_data)
 
-print(f"Self-divergence: {args.network0}")
+print(f"Self-divergence: {name0}")
 imagenet.divergence(network0, network0, train_data)
 
-print(f"Divergence: {args.network0} - {args.network1}")
+print(f"Divergence: {name0} - {name1}")
 imagenet.divergence(network1, network1, train_data)
 
-print(f"Divergence: {args.network0} - inflated({args.network0}, {args.network1})")
+print(f"Divergence: {name0} - inflated({name0}, {name1})")
 inflate.resnet(network0, network1)
 imagenet.divergence(network0, network1, train_data)
