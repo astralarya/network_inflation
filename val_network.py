1cb5ff693dfb6b7104f3caf8077f4c6f9e524550
Mara Kim <mara@autochthe.net>
2023-02-26 16:37:38 -0800

    Val options

--- val_network.py
+++ val_network.py
@@ -4,12 +4,14 @@ parser = argparse.ArgumentParser(
     prog="ResNet validation script"
 )
 parser.add_argument('name')
+parser.add_argument('--inflate', choices=['resnet50', 'resnet101'])
 parser.add_argument('--network', choices=['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152'])
 parser.add_argument('--batch_size', default=64, type=int)
 parser.add_argument('--epoch', default="all", type=lambda x: x if x in ["pre", "init", "all"] else int(x))
 args = parser.parse_args()
 
 
+from local import inflate
 from local import imagenet
 from local import model
 from local import resnet
@@ -17,6 +19,7 @@ from local import resnet
 
 name = args.name
 network = getattr(resnet, args.network, lambda: None)()
+inflate_source = getattr(resnet, args.network, lambda: None)()
 
 if network is None:
     print(f"Unknown network: {args.network}")
@@ -26,6 +29,8 @@ if network is None:
 val_data = imagenet.val_data("/mnt/imagenet/imagenet-1k/val/")
 
 if args.epoch == "pre":
+    if args.inflate is not None:
+        inflate.resnet(inflate_source, network)
     imagenet.run_val(network, name, val_data, batch_size=args.batch_size)
 elif args.epoch == "init":
     model.reset(network)
