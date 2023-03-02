b2c701c9e6ad9dd4977a6a61ada493fa73affe17
Mara Kim <mara@autochthe.net>
2023-03-02 02:10:09 -0800

    Fix

--- local/model.py
+++ local/model.py
@@ -50,7 +50,7 @@ def state_to(state: Any, device: torch.device):
             r[key] = state_to(val, device)
         return r
     elif type(state) == torch.Tensor:
-        return to(device)
+        return to(state)
     else:
         return state
 
