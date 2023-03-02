03a75b208fec7d80df14727277051dacce9648f5
Mara Kim <mara@autochthe.net>
2023-03-02 01:55:54 -0800

    nocopy

--- local/model.py
+++ local/model.py
@@ -49,7 +49,7 @@ def state_to(state: Any, device: torch.device):
             r[key] = state_to(val, device)
         return r
     elif type(state) == torch.Tensor:
-        r = state.to(copy.copy(device))
+        r = state.to(device)
         return r
     else:
         return state
