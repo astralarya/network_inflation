95b4e1d445e3c484f8ec52fe7d57ab7cfd15b06c
Mara Kim <mara@autochthe.net>
2023-03-05 02:26:33 -0800

    Better setup

--- scripts/setup_gcloud.bash
+++ scripts/setup_gcloud.bash
@@ -1,12 +1,10 @@
 # Run on local dev
 
 INSTANCE_NAME="tpu-eu-2"
-
 GCLOUD_ZONE="europe-west4-a"
+
 TPU_TYPE="v3-8"
 TPU_VERSION="tpu-vm-pt-1.13"
-GCLOUD_DISK="imagenet--$INSTANCE_NAME"
-GCLOUD_DISK_SIZE="150GB"
 GCLOUD_SNAPSHOT="imagenet-1k--eu"
 
 gcloud compute tpus tpu-vm create "$INSTANCE_NAME" \
@@ -14,15 +12,29 @@ gcloud compute tpus tpu-vm create "$INSTANCE_NAME" \
     "--accelerator-type=$TPU_TYPE" \
     "--version=$TPU_VERSION"
 
-gcloud compute disks create "$GCLOUD_DISK" \
+# Imagenet
+
+gcloud compute disks create "imagenet--$INSTANCE_NAME" \
     "--zone=$GCLOUD_ZONE" \
-    "--size=$GCLOUD_DISK_SIZE" \
+    "--size=150GB" \
     "--source-snapshot=$GCLOUD_SNAPSHOT"
 
 gcloud alpha compute tpus tpu-vm attach-disk "$INSTANCE_NAME" \
     "--zone=$GCLOUD_ZONE" \
-    "--disk=$GCLOUD_DISK" \
+    "--disk=imagenet--$INSTANCE_NAME" \
     --mode=read-only
+
+# Model checkpoints
+
+gcloud compute disks create "models--$INSTANCE_NAME" \
+    "--zone=$GCLOUD_ZONE" \
+    "--size=200GB"
+
+gcloud alpha compute tpus tpu-vm attach-disk "$INSTANCE_NAME" \
+    "--zone=$GCLOUD_ZONE" \
+    "--disk=models--$INSTANCE_NAME"
+
+# SSH
 
 gcloud compute tpus tpu-vm ssh "$INSTANCE_NAME" \
     "--zone=$GCLOUD_ZONE"
