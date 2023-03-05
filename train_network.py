514fd15771e1677152b52b093f09307e7b21c00c
Mara Kim <mara@autochthe.net>
2023-03-04 22:22:01 -0800

    defaults

--- train_network.py
+++ train_network.py
@@ -9,15 +9,15 @@ parser.add_argument(
     "name",
     choices=["resnet18", "resnet34", "resnet50", "resnet101", "resnet152"],
 )
-parser.add_argument("--num_epochs", default=200, type=int)
+parser.add_argument("--num_epochs", default=600, type=int)
 parser.add_argument("--finetune", action="store_true")
 parser.add_argument("--inflate", choices=["resnet50", "resnet101"])
-parser.add_argument("--lr", default=0.1, type=float)
-parser.add_argument("--momentum", default=0.9, type=float)
-parser.add_argument("--weight_decay", default=0.0001, type=float)
-parser.add_argument("--batch_size", default=128, type=int)
-parser.add_argument("--num_workers", default=4, type=int)
 parser.add_argument("--nprocs", default=8, type=int)
+parser.add_argument("--num_workers", default=4, type=int)
+parser.add_argument("--batch_size", default=128, type=int)
+parser.add_argument("--momentum", default=0.9, type=float)
+parser.add_argument("--lr", default=0.5, type=float)
+parser.add_argument("--weight_decay", default=2e-5, type=float)
 parser.add_argument("--model_path", default="models", type=Path)
 parser.add_argument(
     "--imagenet_path",
