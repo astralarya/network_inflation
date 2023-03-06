2d8f2090ec37a9d0ab615f111665adccfda28dad
Mara Kim <mara@autochthe.net>
2023-03-05 23:19:51 -0800

    More strategies

--- local/inflate.py
+++ local/inflate.py
@@ -1,12 +1,25 @@
-from itertools import zip_longest
+from enum import Enum
+from typing import Union
 
 import torch
 import torch.nn as nn
 from torchvision.models import ResNet
 
 
+class SequenceInflate(Enum):
+    ALIGN_START = 0
+    ALIGN_END = 1
+    CENTER = 2
+    SPACE_EVENLY = 3
+
+
 @torch.no_grad()
-def resnet(network0: ResNet, network1: ResNet, mask=True):
+def resnet(
+    network0: ResNet,
+    network1: ResNet,
+    strategy: SequenceInflate = SequenceInflate.ALIGN_START,
+    mask: Union[str, None] = "conv3.weight",
+):
     """Initialize network1 via inflating network0
 
     Args:
@@ -18,17 +31,46 @@ def resnet(network0: ResNet, network1: ResNet, mask=True):
         copy(network0.get_submodule(child), network1.get_submodule(child))
 
     for layer in ["layer1", "layer2", "layer3", "layer4"]:
-        children0 = network0.get_submodule(layer).children()
-        children1 = network1.get_submodule(layer).children()
-
-        for child0, child1 in zip_longest(children0, children1):
-            if child1 is None:
-                raise Exception("Inflate destination is smaller than source!")
-            elif child0 is None and mask:
-                child1.get_parameter("conv3.weight").zero_()
-            else:
-                copy(child0, child1)
+        inflate_sequence(
+            network0.get_submodule(layer),
+            network1.get_submodule(layer),
+            strategy=strategy,
+            mask=mask,
+        )
     return network1
+
+
+@torch.no_grad()
+def inflate_sequence(
+    sequence0: nn.Module,
+    sequence1: nn.Module,
+    strategy: SequenceInflate,
+    mask: Union[str, None],
+):
+    children0 = list(sequence0.children())
+    children1 = list(sequence1.children())
+    diff = len(children1) - len(children0)
+
+    if strategy == SequenceInflate.ALIGN_START:
+        children0.extend([None] * diff)
+    elif strategy == SequenceInflate.ALIGN_END:
+        children0[1:1] = [None] * diff
+    elif strategy == SequenceInflate.CENTER:
+        children0.extend([None] * (diff // 2 + diff % 2))
+        children0[1:1] = [None] * (diff // 2)
+    elif strategy == SequenceInflate.SPACE_EVENLY and diff > 0:
+        step = (len(children0) - 1) / diff
+        for i in range(1, diff):
+            idx = 1 + round(i * step)
+            children0[idx:idx] = [None]
+
+    for child0, child1 in zip(children0, children1):
+        if child1 is None:
+            raise Exception("Inflate destination is smaller than source!")
+        elif child0 is None and mask:
+            child1.get_parameter(mask).zero_()
+        else:
+            copy(child0, child1)
 
 
 @torch.no_grad()
