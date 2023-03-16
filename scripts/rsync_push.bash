209edb4424876996044fbfd335309342e8c509a6
Mara Kim <mara@autochthe.net>
2023-03-15 21:28:51 -0400

    Specify destination

--- scripts/rsync_push.bash
+++ scripts/rsync_push.bash
@@ -1,13 +1,12 @@
 INSTANCE_ADDR="$1"
 SOURCE_DIR="$2"
+DEST_DIR="$3"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 RSYNC_USER="$RSYNC_USER"
 
 GCLOUD_ZONE=$( dirname "$INSTANCE_ADDR" )
 INSTANCE_NAME=$( basename "$INSTANCE_ADDR" )
-
-DEST_DIR="/mnt/models/$SOURCE_DIR"
 
 
 address="$( \
